518926942ea4b1a580c8e5d69a8d6e21644ea4b2
Appel mahmud akib <Appelmahmud74@gmail.com>
2018-08-29 21:08:25 +0600

    aboutus donor commit

--- app/src/main/java/com/experiment/appel/blooddonor/AboutUs.java
+++ app/src/main/java/com/experiment/appel/blooddonor/AboutUs.java
@@ -1,7 +1,10 @@
 package com.experiment.appel.blooddonor;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class AboutUs extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class AboutUs extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+    }
+
+    public void contactDev(View view) {
+
+        Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
+                "mailto","appelmahmud74@gmail.com", null));
+        startActivity(emailIntent);
     }
 }
