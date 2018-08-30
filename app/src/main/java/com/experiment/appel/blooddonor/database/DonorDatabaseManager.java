82d5f3d186eb1da3cbc5a17f516c64d9f72adec2
Appel mahmud akib <Appelmahmud74@gmail.com>
2018-08-30 19:14:44 +0600

    Update DonorDatabaseManager.java

--- app/src/main/java/com/experiment/appel/blooddonor/database/DonorDatabaseManager.java
+++ app/src/main/java/com/experiment/appel/blooddonor/database/DonorDatabaseManager.java
@@ -57,6 +57,45 @@ public class DonorDatabaseManager {
         }
         return donors;
     }
+public long updateDonor(Donor donor){
+        SQLiteDatabase sqLiteDatabase=databaseHelper.getWritableDatabase();
+        ContentValues contentValues=new ContentValues();
+        contentValues.put(DatabaseHelper.COLUMN_DONOR_NAME,donor.getName());
+        contentValues.put(DatabaseHelper.COLUMN_DONOR_AGE,donor.getAge());
+        contentValues.put(DatabaseHelper.COLUMN_DONOR_GENDER,donor.getGender());
+        contentValues.put(DatabaseHelper.COLUMN_DONOR_ADDRESS,donor.getAddress());
+        contentValues.put(DatabaseHelper.COLUMN_DONOR_CONTACT_NO,donor.getContactNo());
+        contentValues.put(DatabaseHelper.COLUMN_DONOR_BLOOD_GROUP,donor.getBloodGroup());
+        contentValues.put(DatabaseHelper.COLUMN_DONOR_LAST_DONATION_DATE,donor.getLastDonationDate());
+        long updatedRow=sqLiteDatabase.update(DatabaseHelper.TABLE_NAME,contentValues,
+                DatabaseHelper.COLUMN_DONOR_ID+" =? ",new String[]{String.valueOf(donor.getId())});
+        return updatedRow;
+    }
 
+    public Donor getDonorById(int id){
+        SQLiteDatabase sqLiteDatabase=databaseHelper.getReadableDatabase();
+        String selectQuery="select * from "+DatabaseHelper.TABLE_NAME
+                +" where id = "+id;
+        Cursor cursor=sqLiteDatabase.rawQuery(selectQuery,null);
+        Donor donor=null;
+        if(cursor.moveToFirst()){
+            String name = cursor.getString(cursor.getColumnIndex(DatabaseHelper.COLUMN_DONOR_NAME));
+            String age = cursor.getString(cursor.getColumnIndex(DatabaseHelper.COLUMN_DONOR_AGE));
+            String gender = cursor.getString(cursor.getColumnIndex(DatabaseHelper.COLUMN_DONOR_GENDER));
+            String address = cursor.getString(cursor.getColumnIndex(DatabaseHelper.COLUMN_DONOR_ADDRESS));
+            String contactNo = cursor.getString(cursor.getColumnIndex(DatabaseHelper.COLUMN_DONOR_CONTACT_NO));
+            String bloodGroup = cursor.getString(cursor.getColumnIndex(DatabaseHelper.COLUMN_DONOR_BLOOD_GROUP));
+            String lastDonationDate = cursor.getString(cursor.getColumnIndex(DatabaseHelper.COLUMN_DONOR_LAST_DONATION_DATE));
+            donor=new Donor(id,name,age,gender,address,contactNo,bloodGroup,lastDonationDate);
+        }
+        return donor;
+    }
+
+    public long deleteDonor(int id){
+        SQLiteDatabase sqLiteDatabase=databaseHelper.getWritableDatabase();
+        long deletedRow=sqLiteDatabase.delete(DatabaseHelper.TABLE_NAME,DatabaseHelper.COLUMN_DONOR_ID+" =? ",new String[]{String.valueOf(id)});
+        sqLiteDatabase.close();
+        return deletedRow;
+    }
 
 }
